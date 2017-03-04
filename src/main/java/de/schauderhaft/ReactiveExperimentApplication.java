69ac4c682b39f4f0a0f814af6998f7bc744ec8af
Jens Schauder <jschauder@pivotal.io>
2017-03-04 13:17:18 +0100

    intial commit

---
+++ src/main/java/de/schauderhaft/ReactiveExperimentApplication.java
@@ -0,0 +1,12 @@
+package de.schauderhaft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveExperimentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveExperimentApplication.class, args);
+	}
+}
